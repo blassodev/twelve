bacfc6344fe092dd3fc42ce37a11f8ee7f8ce840
pexugadepollo <bso080601@gmail.com>
2021-09-17 09:23:43 +0200

    Configuracion final de prisma en docker

--- server/index.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-
-const a = async () => {
-    const users = await prisma.users.findMany();
-    console.log(users)
-}
-
-a();
