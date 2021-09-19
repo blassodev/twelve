ee82fa765eccaa9be3e25d5c926dab3ca4c00501
Blas Santomé <bso080601@gmail.com>
2021-09-19 23:08:54 +0200

    Configured and installed apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,31 @@
 import { PrismaClient } from '@prisma/client'
+import { ApolloServer, gql } from 'apollo-server'
 
 const prisma = new PrismaClient()
 
-async function main() {
-    console.log(await prisma.users.findMany())
+const typeDefs = gql`
+type User {
+    username: String
+    email: String
+}
+
+type Query {
+    users: [User]
+}
+`;
+
+
+
+const main = async () => {
+    const resolvers = {
+        Query: {
+            users: async () => await prisma.users.findMany()
+        }
+    }
+    const server = new ApolloServer({typeDefs, resolvers})
+    server.listen().then(({url})=>{
+        console.log(`🚀  Server ready at ${url}`);
+    })
 }
 
 main()
@@ -12,4 +34,4 @@ main()
     })
     .finally(async () => {
         await prisma.$disconnect()
-    })
+    });
